c3f70aaf69621486b6613081a5f9a4f921f155c0
hmasila <hannahmasila@gmail.com>
2024-06-13 19:57:07 -0400

    create seeds to populate db with random contacts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# We could use faker gem here but since it's a small app, let's just generate some randoms
+# Clear existing data
+Contact.destroy_all
+
+# Function to generate random names
+def random_name
+  first_names = ["John", "Jane", "Alex", "Chris", "Pat", "Taylor", "Jordan", "Morgan"]
+  last_names = ["Smith", "Doe", "Brown", "Johnson", "Lee", "Walker", "Hall", "Young"]
+  "#{first_names.sample} #{last_names.sample}"
+end
+
+def random_phone
+  Array.new(10) { rand(0..9) }.join
+end
+
+def random_email(name)
+  domains = ["example.com", "mail.com", "test.org", "email.net"]
+  "#{name.downcase.gsub(' ', '.')}@#{domains.sample}"
+end
+
+# Create sample contacts
+20.times do
+  name = random_name
+  Contact.create!(
+    name: name,
+    email: random_email(name),
+    phone: random_phone
+  )
+end
+
+puts "Created 20 contacts"
